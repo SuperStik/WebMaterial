f1b4c1352d2c54f4001118e29737b359c09e76b0
SuperStik <daviswood290@gmail.com>
2021-07-27 14:41:43 -0700

    Create prelimary WebMaterial function

--- lua/autorun/init_webmat.lua
+++ lua/autorun/init_webmat.lua
@@ -10,6 +10,10 @@ file.CreateDir("webmats")
 AddCSLuaFile("modules/webmat.lua")
 include("modules/webmat.lua")
 
+function WebMaterial(uri, png)
+	Material("../data/" .. webmat.Download(uri), png)
+end
+
 hook.Add("ShutDown", "RemoveWebMats", function()
 	if file.Exists("webmats/", "DATA") then
 		for k, v in pairs(file.Find("webmats/*", "DATA")) do
