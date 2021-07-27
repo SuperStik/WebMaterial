fad938ea5eea2342bde09541883a385af68e16e0
SuperStik <daviswood290@gmail.com>
2021-07-27 10:25:27 -0700

    Use locals

--- lua/modules/webmat.lua
+++ lua/modules/webmat.lua
@@ -1,14 +1,18 @@
 local SERVER = SERVER
+local HTTP = HTTP
 local SHA1 = util.SHA1
+local writeFile = file.Write
+local ErrorNoHalt = ErrorNoHalt
+local error = error
 
 module("webmat")
 
 local function successjpeg(code, body)
-	file.Write("webmats/" .. SHA1(body) .. ".jpg", body)
+	writeFile("webmats/" .. SHA1(body) .. ".jpg", body)
 end
 
 local function successpng(code, body)
-	file.Write("webmats/" .. SHA1(body) .. ".png", body)
+	writeFile("webmats/" .. SHA1(body) .. ".png", body)
 end
 
 if SERVER then -- will be used to network the uri when a client joins
