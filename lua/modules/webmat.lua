d82f3ad8c8fea33c5ec925a4442679754ecd74ab
SuperStik <daviswood290@gmail.com>
2021-07-27 10:31:14 -0700

    Removed unnecessary conditionals

--- lua/modules/webmat.lua
+++ lua/modules/webmat.lua
@@ -1,10 +1,8 @@
-local SERVER = SERVER
 local HTTP = HTTP
 local SHA1 = util.SHA1
 local writeFile = file.Write
 local ErrorNoHalt = ErrorNoHalt
 local error = error
-
 module("webmat")
 
 local function successjpeg(code, body)
@@ -15,48 +13,24 @@ local function successpng(code, body)
 	writeFile("webmats/" .. SHA1(body) .. ".png", body)
 end
 
-if SERVER then -- will be used to network the uri when a client joins
-	local nettbl = {}
+function Download(uri)
+	local ext = uri:Right(4)
 
-	function Download(uri, save)
-		local ext = uri:Right(4)
-		if ext == ".jpg" or uri:Right(5) == ".jpeg" then
-			HTTP({
-				failed = ErrorNoHalt,
-				success = successjpeg,
-				method = "GET",
-				url = uri
-			})
-		elseif ext == ".png" then
-			HTTP({
-				failed = ErrorNoHalt,
-				success = successpng,
-				method = "GET",
-				url = uri
-			})
-		else
-			error("Invalid URI entered for webmat.Download!")
-		end
-	end
-else
-	function Download(uri)
-		local ext = uri:Right(4)
-		if ext == ".jpg" or uri:Right(5) == ".jpeg" then
-			HTTP({
-				failed = ErrorNoHalt,
-				success = successjpeg,
-				method = "GET",
-				url = uri
-			})
-		elseif ext == ".png" then
-			HTTP({
-				failed = ErrorNoHalt,
-				success = successpng,
-				method = "GET",
-				url = uri
-			})
-		else
-			error("Invalid URI entered for webmat.Download!")
-		end
+	if ext == ".jpg" or uri:Right(5) == ".jpeg" then
+		HTTP({
+			failed = ErrorNoHalt,
+			success = successjpeg,
+			method = "GET",
+			url = uri
+		})
+	elseif ext == ".png" then
+		HTTP({
+			failed = ErrorNoHalt,
+			success = successpng,
+			method = "GET",
+			url = uri
+		})
+	else
+		error("Invalid URI entered for webmat.Download!")
 	end
 end
